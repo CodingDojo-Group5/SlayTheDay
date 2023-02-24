58c7c77da02d87afa0d007b74c064c6d324f29ec
LeonardoMateo1 <104122352+LeonardoMateo1@users.noreply.github.com>
2023-02-24 06:26:05 -0500

    Landing Page Frontend / Backend

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+    import React, {useState} from 'react';
+    import {useNavigate, Link} from 'react-router-dom'
+    import bounce from '../assets/bounce-landing.mp4'
+    import axios from 'axios';
+    
+    
+    const Landing = () => {
+    
+        const [state,setState] = useState({
+        login: {
+            email: "",
+            password: ""
+        }
+    })
+    const navigate = useNavigate();
+    const {login} = state;
+    const [errors, setErrors] = useState({})
 
-const Landing = () => {
-  return (
-    <div>Landing</div>
-  )
+
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        axios.post("http://localhost:8000/api/user/:id/login", login, {withCredentials: true})
+            .then((res) => {console.log(res); navigate('/dashboard')})
+            .catch((err) => {
+                console.log(err)
+                setErrors(err.response.data);
+            })
+    }
+    
+    const handleLoginInputs = (e) => {
+        setState({...state, login: {...state.login, [e.target.name]: e.target.value}})
+    }
+
+
+
+    return (
+    <div className='landing-b'>
+        <div className="lb-left">
+            <video className='bounce' src={bounce} autoPlay loop muted/>
+            <p className="c-name">
+                <span>SlayTheDay</span>
+            </p>
+            <p className='hero'>A place to empower individuals to conquer their daily tasks with a </p>
+            <p className='hero'>platform that simplifies task management and organization.</p>
+        </div>
+        <div className="lb-right">
+            <form className='l-form' onSubmit={handleLogin}>
+                
+                <div>
+                    <input onChange={handleLoginInputs} name='email' type="text" placeholder='Email'/><br />
+                    <div className='errors'>{errors && (errors.message || (errors.data && errors.data.message)) ? (errors.message || errors.data.message) : null}</div>
+                </div>
+                <div>
+                    <input onChange={handleLoginInputs} name='password' type="text" placeholder='Password'/>
+                </div>
+                <input type="submit" value="Log In" className='l-btn'/>
+                <div className="line"></div>
+                <Link to='/create'><button className='l-c-btn'>Create</button></Link>
+            </form>
+        </div>
+    </div>
+    )
 }
 
 export default Landing
