a3d8b269f639f7d49b0bb9f7b805b3150f4fce2a
Dylan Stalcar <99678496+dylandotanthony@users.noreply.github.com>
2023-02-26 14:55:59 -0700

    Dylan1 branch (#15)
    * added components
    
    * added form data and toggle
    
    * added auth and worked on delete,
    
    * added map functions and worked on updates function

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Main from './views/Main';
 import CreateTask from './components/CreateTask';
 import Landing from './components/Landing';
 import Create from './components/Create';
+import Update from './views/Update'
 
 
 function App() {
@@ -20,6 +21,8 @@ function App() {
           <Route path='/create' element={<Create />} />
           <Route element={<Main />} path="/user/todos" />
           <Route element={<CreateTask />} path='/todo/new' />
+          <Route element={< Update />} path='todo/:id/update' />
+
         </Routes>
       </div>
     </BrowserRouter>
--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -1,25 +1,27 @@
 import * as React from 'react';
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { Link, useNavigate } from "react-router-dom";
-import { Card, Form, Row, Col, Container } from 'react-bootstrap'
+import { Row, Col, Container, Button } from 'react-bootstrap'
 
 
 
 const TaskList = (props) => {
     const { todos, setTodos } = props;
 
+
     useEffect(() => {
         axios.get("http://localhost:8000/api/user/todos", { withCredentials: true })
             .then((res) => {
                 console.log(res.data);
-                setTodos(res.data);
+                setTodos(res.data.user.todos);
             })
             .catch((err) => console.log(err));
     }, [])
 
-
-
+    const notStarted = todos.filter(task => task.todoStatus == 'not-started');
+    const inProgress = todos.filter(task => task.todoStatus == 'in-progress');
+    const completed = todos.filter(task => task.todoStatus == 'completed');
 
     return (
         <div style={{ width: '800px', marginLeft: '400px' }}>
@@ -28,25 +30,61 @@ const TaskList = (props) => {
             <hr />
             <Container>
                 <Row >
-
                     <Col>
                         <h2>Not Completed</h2>
-
-
+                        {notStarted.map((task, index) =>
+                        (
+                            <div key={index} className='d-flex p-2 justify-content-evenly'>
+                                <p>
+                                    {task.task}
+                                </p>
+                                <Link to={`/todo/${task._id}/update`}>
+                                    <Button variant="outline-secondary">
+                                        Update
+                                    </Button>{' '}
+                                </Link>
+                            </div>
+                        ))}
                     </Col>
 
                     <Col>
                         <h2>In Progress</h2>
+                        {inProgress.map((task, index) =>
+                        (
+                            <div key={index} className='d-flex p-2 justify-content-evenly'>
+                                <p>
+                                    {task.task}
+                                </p>
+                                <Link to={`/todo/${task._id}/update`}>
+                                    <Button variant="outline-secondary">
+                                        Update
+                                    </Button>{' '}
+                                </Link>
+                            </div>
+
+                        ))}
 
                     </Col>
                     <Col>
                         <h2> Completed</h2>
+                        {completed.map((task, index) =>
+                        (
+                            <div key={index} className='d-flex p-2 justify-content-evenly'>
+                                <p>
+                                    {task.task}
+                                </p>
+                                <Link to={`/todo/${task._id}/update`}>
+                                    <Button variant="outline-secondary">
+                                        Update
+                                    </Button>{' '}
+                                </Link>
+                            </div>
+
+                        ))}
 
                     </Col>
                 </Row>
             </Container>
-
-
         </div>
     )
 }
