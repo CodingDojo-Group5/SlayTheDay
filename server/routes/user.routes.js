b65e5f37af08722d7ff01198244697547101f909
Jeff Steele <steelem81@gmail.com>
2023-02-26 19:11:05 -0700

    added route to get one todo

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -7,7 +7,8 @@ module.exports = (app) => {
     app.post("/api/user/login", UserController.login) 
     app.get("/api/user/logout", UserController.logout)
     app.post("/api/todo/new", authenticate, TodoController.createTodo)
-    app.get("/api/user/todos", authenticate, UserController.getUserTodos) 
+    app.get("/api/user/todos", authenticate, UserController.getUserTodos)
+    app.get("api/todo/:id", authenticate, TodoController.getOneTodo) 
     app.put("/api/todo/:id/update", authenticate, TodoController.updateTodo)
     app.delete("/api/todo/:id/delete", authenticate, TodoController.deleteTodo)
 }
